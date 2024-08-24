b3e3c9d60b4d022efb8b1f1d489e47f726254f5b
domin <2@wp.pl>
2024-08-24 18:35:47 +0200

    Integer to Binary String

--- src/_1Lesson/BinaryGap.java
+++ src/_1Lesson/BinaryGap.java
@@ -1,4 +1,20 @@
 package _1Lesson;
 
 public class BinaryGap {
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int N){
+        int binaryGap =0;
+
+        String binary = Integer.toBinaryString(N);
+
+
+        return binaryGap;
+
+    }
+
+
+
 }
