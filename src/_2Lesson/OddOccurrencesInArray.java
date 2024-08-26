f4d3ba8cf422217312c0058154e47961fb6707fc
domin <2@wp.pl>
2024-08-26 14:37:00 +0200

    Finished

--- src/_2Lesson/OddOccurrencesInArray.java
+++ src/_2Lesson/OddOccurrencesInArray.java
@@ -1,4 +1,22 @@
 package _2Lesson;
 
 public class OddOccurrencesInArray {
+
+    public static void main(String[] args) {
+        int[] A = {1, 2, 3, 3, 1};
+        int[] B = {9, 3, 9, 3, 3, 9, 7, 9};
+
+        System.out.println(solution(A));
+        System.out.println(solution(B));
+    }
+
+    public static int solution(int[] A) {
+        int n = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            n ^= A[i];
+        }
+        return n;
+    }
+
 }
