6f3c75748c9c3948da93248c889d8900cb5efc42
domin <2@wp.pl>
2024-08-26 15:42:47 +0200

    Finished. Sort & find missing

--- src/_3Lesson/PermMissingElem.java
+++ src/_3Lesson/PermMissingElem.java
@@ -1,4 +1,24 @@
 package _3Lesson;
 
-public class PermMissingElem {
+import java.util.Arrays;
+
+public class PermMissingElem{
+    public static void main(String[] args) {
+        int[] A = {1};
+        System.out.println(missingElement(A));
+
+    }
+
+    public static int missingElement(int[] A) {
+        Arrays.sort(A);
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return A.length + 1;
+    }
 }
+
+
